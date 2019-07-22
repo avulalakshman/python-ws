2977fb8b5a6a4bc933788fad5e4e0f883f07c26d
lakshman.avula@outlook.com <lakshman.avula@outlook.com>
2019-07-22 14:22:41 +0530

    Refactored code using functions

--- M1_Q/q_1.py
+++ M1_Q/q_1.py
@@ -1,16 +1,18 @@
 '''1.	Write a program to accept a number and determine whether it is a prime number or not.'''
 import math
 
-num = int(input("Enter the number: "))
-is_prime = True
-if num < 2:
-    is_prime = False
-else:
-    for i in range(2, int(math.sqrt(num)) + 1):
-        if num % i == 0:
-            is_prime = False
-            break
+def is_prime_fun(num):
+    ''' If given num is prime it return True otherwise False '''
+    if num < 2:
+       return False
+    else:
+        for i in range(2, int(math.sqrt(num)) + 1):
+            if num % i == 0:
+                return False
+    return True
 
+num = int(input("Enter the number: "))
+is_prime = is_prime_fun(num)
 if is_prime:
     print(f"{num} is a prime number")
 else:
